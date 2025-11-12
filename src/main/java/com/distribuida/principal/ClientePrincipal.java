de3eed047b42a0a32825f7179acb095965380b3b
1720591989 <dmaiquez@tecnologicoismac.edu.ec>
2025-11-12 12:18:09 -0500

    cambios 2

--- src/main/java/com/distribuida/principal/ClientePrincipal.java
+++ src/main/java/com/distribuida/principal/ClientePrincipal.java
@@ -6,10 +6,12 @@ public class ClientePrincipal {
 
     public static void main(String[] args){
 
+            ///  ejemplos de model
+
         Cliente cliente = new Cliente(1, "1701234567","Juan"
                 ,"Taipe","av por ahi", "0987654321","jtaipe@correo.com");
 
-        ClienteRepository clienteRepository;
+
 
         System.out.println(cliente.toString());
     }
