65486c327429644d9df5fcfb9f9376f9d8bae512
1720591989 <dmaiquez@tecnologicoismac.edu.ec>
2025-11-12 12:20:49 -0500

    cambios 3

--- src/main/java/com/distribuida/principal/ClientePrincipal.java
+++ src/main/java/com/distribuida/principal/ClientePrincipal.java
@@ -11,7 +11,7 @@ public class ClientePrincipal {
         Cliente cliente = new Cliente(1, "1701234567","Juan"
                 ,"Taipe","av por ahi", "0987654321","jtaipe@correo.com");
 
-
+    /// / otro ejemplo de otro codigo
 
         System.out.println(cliente.toString());
     }
